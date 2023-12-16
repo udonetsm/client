caf7c75dfb1b9119382a09b29d8d765fd5106e0b
Maksim Donets <maks.donets.95@outlook.com>
2023-12-16 10:55:20 +0300

    add comments

--- models/models.go
+++ models/models.go
@@ -1,5 +1,8 @@
 package models
 
+// This package can be imported as github.com/udonetsm/client/models.
+// Server side uses it
+
 import (
 	"encoding/json"
 	"fmt"
@@ -11,6 +14,7 @@ import (
 	"gorm.io/gorm"
 )
 
+// this object for load database connection config
 type YAMLObject struct {
 	Host string `yaml:"host"`
 	Port string `yaml:"port"`
@@ -20,11 +24,13 @@ type YAMLObject struct {
 	DBNM string `yaml:"dbname"`
 }
 
+// duck typing for load data base connection config
 type CfgDBGetter interface {
 	YAMLCfg(string)
 	GetDB() *gorm.DB
 }
 
+// this method read config from target .yaml file and unpack it in object
 func (y *YAMLObject) YAMLCfg(path string) {
 	data, err := os.ReadFile(path)
 	if err != nil {
@@ -33,6 +39,9 @@ func (y *YAMLObject) YAMLCfg(path string) {
 	err = yaml.Unmarshal(data, y)
 }
 
+// build database connection string
+// using object built on YAMLCfg function
+// and get database usin built config
 func (y *YAMLObject) GetDB() (db *gorm.DB) {
 	var err error
 	dsn := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=%s", y.User, y.Pass, y.Host, y.Port, y.DBNM, y.SSLM)
@@ -43,28 +52,40 @@ func (y *YAMLObject) GetDB() (db *gorm.DB) {
 	return
 }
 
+// using duck typing for load database connection
 func LoadCfgAndGetDB(yg CfgDBGetter, path string) (db *gorm.DB) {
 	yg.YAMLCfg(path)
 	db = yg.GetDB()
 	return
 }
 
+// Contact object
 type Contact struct {
 	Number     string   `json:"num,omitempty"`
 	Name       string   `json:"name,omitempty"`
 	NumberList []string `json:"nlist,omitempty"`
 }
 
+// JSON object for making request to server side
+// includes:
+// target for fill entry_id in database
+// Contact for build json string for use functions on the server side
 type JSONObject struct {
-	Number string  `json:"target"`
-	Object Contact `json:"object,omitempty"`
+	Number string `json:"target"`
+	// Object can be empty if using the DeleteOrInfo function.
+	// See package github.com/udonetsm/client/http
+	Object *Contact `json:"object,omitempty"`
 }
 
+// Duck typing for json object
 type PackUnpacker interface {
 	Pack() []byte
+	// Unpack for use it on the server side
+	// This func unpacking json on the server side
 	Unpack()
 }
 
+// Pack object to json string
 func (j *JSONObject) Pack() (data []byte) {
 	data, err := json.Marshal(j)
 	if err != nil {
@@ -73,6 +94,7 @@ func (j *JSONObject) Pack() (data []byte) {
 	return
 }
 
+// Unpack object from json string to JSONObject struct
 func (j *JSONObject) Unpack(data []byte) {
 	err := json.Unmarshal(data, j)
 	if err != nil {
@@ -80,11 +102,13 @@ func (j *JSONObject) Unpack(data []byte) {
 	}
 }
 
+// Use duck typing for pack
 func Packing(pu PackUnpacker) (data []byte) {
 	data = pu.Pack()
 	return
 }
 
+// Use duck typing for unpack
 func Unpacking(pu PackUnpacker) {
 	pu.Unpack()
 	return
