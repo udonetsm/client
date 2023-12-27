cac67cba6da49335d7a8637893523f7d33a2fc04
Maksim Donets <maks.donets.95@outlook.com>
2023-12-27 14:00:55 +0300

    move database methods to serverside, rebuild Contact and Entries add error field

--- models/models.go
+++ models/models.go
@@ -5,58 +5,47 @@ package models
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
-	"os"
-
-	"gopkg.in/yaml.v2"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
 
-// this object for load database connection config
-type YAMLObject struct {
-	Host string `yaml:"host"`
-	Port string `yaml:"port"`
-	User string `yaml:"user"`
-	Pass string `yaml:"password"`
-	SSLM string `yaml:"sslmode"`
-	DBNM string `yaml:"dbname"`
+// JSON object for making request to server side
+// includes:
+// target for fill entry_id in database
+// Contact for build json string for use functions on the server side
+type Entries struct {
+	Number string `json:"target"`
+	// Object can be empty if using the DeleteOrInfo function.
+	// See package github.com/udonetsm/client/http
+	Object string `json:"object,omitempty"`
+	Error  error  `gorm:"-" json:"omitempty"`
 }
 
-// duck typing for load data base connection config
-type CfgDBGetter interface {
-	YAMLCfg(string)
-	GetDB() *gorm.DB
-}
-
-// this method read config from target .yaml file and unpack it in object
-func (y *YAMLObject) YAMLCfg(path string) {
-	data, err := os.ReadFile(path)
+// Pack object to json string
+func (j *Entries) Pack(contact *Contact) (data []byte) {
+	data, err := json.Marshal(contact)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		j.Error = err
+		return
 	}
-	err = yaml.Unmarshal(data, y)
-}
-
-// build database connection string
-// using object built on YAMLCfg function
-// and get database usin built config
-func (y *YAMLObject) GetDB() (db *gorm.DB) {
-	var err error
-	dsn := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=%s", y.User, y.Pass, y.Host, y.Port, y.DBNM, y.SSLM)
-	db, err = gorm.Open(postgres.Open(dsn))
+	j.Object = string(data)
+	data, err = json.Marshal(j)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		j.Error = err
+		return
 	}
 	return
 }
 
-// using duck typing for load database connection
-func LoadCfgAndGetDB(yg CfgDBGetter, path string) (db *gorm.DB) {
-	yg.YAMLCfg(path)
-	db = yg.GetDB()
-	return
+// Unpack object from json string to JSONObject struct
+func (j *Entries) Unpack(data []byte) {
+	err := json.Unmarshal(data, j)
+	if err != nil {
+		log.Println(err)
+		j.Error = err
+		return
+	}
 }
 
 // Contact object
@@ -64,12 +53,15 @@ type Contact struct {
 	Number     string   `json:"num,omitempty"`
 	Name       string   `json:"name,omitempty"`
 	NumberList []string `json:"nlist,omitempty"`
+	Error      error    `json:"error,omitempty"`
 }
 
 func (c *Contact) Unpack(data []byte) {
 	err := json.Unmarshal(data, c)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		c.Error = err
+		return
 	}
 }
 
@@ -81,49 +73,12 @@ func UnpackingContact(p PackUnpackerContact, data []byte) {
 	p.Unpack(data)
 }
 
-// JSON object for making request to server side
-// includes:
-// target for fill entry_id in database
-// Contact for build json string for use functions on the server side
-type Entries struct {
-	Number string `json:"target"`
-	// Object can be empty if using the DeleteOrInfo function.
-	// See package github.com/udonetsm/client/http
-	Object string `json:"object,omitempty"`
-}
-
 // Duck typing for json object
 type PackUnpacker interface {
 	Pack(*Contact) []byte
 	// Unpack for use it on the server side
 	// This func unpacking json on the server side
 	Unpack([]byte)
-}
-
-// Pack object to json string
-func (j *Entries) Pack(contact *Contact) (data []byte, err error) {
-	data, err = json.Marshal(contact)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	j.Object = string(data)
-	data, err = json.Marshal(j)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	return
-}
-
-// Unpack object from json string to JSONObject struct
-func (j *Entries) Unpack(data []byte) (err error) {
-	err = json.Unmarshal(data, j)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	return
 }
 
 // Use duck typing for pack
